c2c2f3bb92b70f553510908b6e53246c91462d24
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-09-30 16:54:55 -0600

    Initial Commit

---
+++ AboutMeiOS22/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMeiOS22
+//
+//  Created by Ivan Ramirez on 9/30/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
